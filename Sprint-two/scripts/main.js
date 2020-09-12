01e989e0c6c936017edede58bb2efa702fe54ac2
Kaleb Armbrust <armbrustcopyco@gmail.com>
2020-09-12 16:11:58 -0700

    beginning of functionality

--- Sprint-two/scripts/main.js
+++ Sprint-two/scripts/main.js
@@ -1,72 +1,46 @@
-const addComment = document.querySelector(".commentsForm");
-const list = document.querySelector('.comments-previous');
+const urlComments = "https://project-1-api.herokuapp.com/comments?api_key=c96c0abc-3e8f-46ca-9145-93c0f269da4f";
+const urlDelete = "https://project-1-api.herokuapp.com/comments/:commendId?api_key=c96c0abc-3e8f-46ca-9145-93c0f269da4f"
 
-addComment.addEventListener('submit', function(e){
-    e.preventDefault();
-    const name = addComment.querySelector('input[type="text"]').value;
-    const comment = addComment.querySelector('.commentBox2').value;
-    console.log(name);
-    console.log(comment);
-    const div = document.createElement('div');
-    const h4 = document.createElement('h4');
-    const p = document.createElement('p');
-    h4.textContent = name;
-    p.textContent = comment;
-    div.appendChild(h4);
-    div.appendChild(p);
-    list.appendChild(div);
-});
+axios
+.get(urlComments)
+.then(res => {
+   console.log(res.data);
 
-window.addEventListener('DOMContentLoaded',() => {
-    const currentComment1 = document.createElement('h3');
-    const currentComment2 = document.createElement("p");
-    currentComment1.innerText = "Michael Lyons 12/18/2018";
-    currentComment2.innerText = "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.";
-    currentComment1.style.marginRight = "auto";
-    currentComment1.style.marginBottom = "0";
-    list.appendChild(currentComment1).value;
-    list.appendChild(currentComment2).value;
-});
+   res.data.forEach(comment => {
+      createComment(comment);
+   })
+})
+.catch(err => console.log(err))
 
-window.addEventListener('DOMContentLoaded',() => {
-    const currentComment1 = document.createElement('h3');
-    const currentComment2 = document.createElement("p");
-    currentComment1.innerText = "Gary Wong 12/12/2018";
-    currentComment2.innerText = "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself.";
-    currentComment1.style.marginRight = "auto";
-    currentComment1.style.marginBottom = "0";
-    list.appendChild(currentComment1).value;
-    list.appendChild(currentComment2).value;
-});
+const form = document.querySelector('.comments-add__form');
+form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            // createComment(comment);
+    })
 
-window.addEventListener('DOMContentLoaded',() => {
-    const currentComment1 = document.createElement('h3');
-    const currentComment2 = document.createElement("p");
-    currentComment1.innerText = "Theodore Duncan 11/15/2018";
-    currentComment2.innerText = "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever.";
-    currentComment1.style.marginRight = "auto";
-    currentComment1.style.marginBottom = "0";
-    list.appendChild(currentComment1).value;
-    list.appendChild(currentComment2).value;
-});
-
-
-
-// let defaultComment = [
-//     {name: "Michael Lyons", timestamp: "12/18/2018", comment: "They Blew the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."},
-//     {name: "Gary Wong", timestamp: "12/12/2018", comment: "Every time I see him shred I feel so motivated to get off my couch andhop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!"},
-//     {name: "Theodore Duncan", timestamp: "11/15/2018", comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitely my favorite ever!"}
-// ]
-
+    function createComment(comment) {
+       const previous = document.querySelector('.comments-previous');
+       const commentContainer = document.createElement('div');
+       const commentImage = document.createElement('img');
+       const commentName = document.createElement('h3');
+       const commentTimestamp = document.createElement('h4');
+       const commentText = document.createElement('p');
+       commentName.innerText = comment.name;
+       commentTimestamp.innerText = comment.timestamp;
+       commentText.innerText = comment.comment;
+       previous.appendChild(commentContainer);
+       commentContainer.appendChild(commentImage);
+       commentContainer.appendChild(commentName);
+       commentContainer.appendChild(commentTimestamp);
+       commentContainer.appendChild(commentText);
+       commentContainer.classList.add('comments-previous__container');
+       commentImage.classList.add('comments-previous__container-image');
+       commentTimestamp.classList.add('comments-previous__container-timestamp');
+       commentName.classList.add('comments-previous__container-name');
+       commentText.classList.add('comments-previous__container-text');
+    }
 // const addComment = document.querySelector(".commentsForm");
+// const list = document.querySelector('.comments-previous');
+// const name = addComment.querySelector('input[type="text"]').value;
+//     const comment = addComment.querySelector('.commentBox2').value;
 
-// window.addEventListener("DOMContentLoaded", displayComment);
-
-// function displayComment(name, comment) {
-//     addComment.addEventListener('submit', function(e){
-//         e.preventDefault();
-//         e.name.target.value;
-//         e.comment.target.value;
-//         defaultComment.unshift(displayComment);
-//         defaultComment.pop();
-// })}
--- Sprint-two/scripts/shows.js
+++ Sprint-two/scripts/shows.js
@@ -1,3 +1,15 @@
+const urlShows = 'https://project-1-api.herokuapp.com/showdates?api_key=?api_key=c96c0abc-3e8f-46ca-9145-93c0f269da4f';
+
+let ax = axios
+.get(urlShows)
+.then(res => {
+
+    res.data.forEach(show => {
+        createCard(show)
+    })
+})
+.catch(err => console.log(err))
+
 window.addEventListener('DOMContentLoaded',() => {
     const labels = document.querySelector(".shows-labels");
     const labelsDates = document.createElement('h3');
@@ -6,188 +18,33 @@ window.addEventListener('DOMContentLoaded',() => {
     labelsDates.innerText = "Dates";
     labelsVenue.innerText = "Venue";
     labelsLocation.innerText = "Location";
-    labelsDates.style.color = "#AFAFAF"
-    labelsVenue.style.color = "#AFAFAF"
-    labelsLocation.style.color = "#AFAFAF"
-    labels.style.marginLeft = "30%";
-    labels.style.marginRight = "0";
-    labels.style.display = 'flex';
-    labels.style.justifyContent = "space-between";
-    labelsVenue.style.marginLeft = "20%";
     labels.appendChild(labelsDates).value;
     labels.appendChild(labelsVenue).value;
     labels.appendChild(labelsLocation).value;
+    labelsDates.classListadd('shows-labels__dates')
+    labelsVenue.classListadd('shows-labels__venue')
+    labelsLocation.classListadd('shows-labels__location')
 });
 
-window.addEventListener('DOMContentLoaded',() => {
-    const grid = document.querySelector(".shows-grid1");
+function createCard(show) {
+    const grid = document.querySelector(".shows-grid");
+    const gridContainer = document.createElement('div');
     const gridDates = document.createElement('h4');
     const gridVenue = document.createElement("p");
     const gridLocation = document.createElement("p");
     const gridButton = document.createElement('button');
-    gridDates.innerText = "Mon Dec 17 2018";
-    gridVenue.innerText = "Ronald Lane";
-    gridLocation.innerText = "San Fransisco, Ca";
+    gridDates.innerText = show.date;
+    gridVenue.innerText = show.location;
+    gridLocation.innerText = show.place;
     gridButton.innerText = "Buy Tickets";
-    grid.style.marginLeft = "60%";
-    grid.style.marginRight = "0";
-    grid.style.display = "flex";
-    grid.style.justifyContent = "space-between"
-    gridVenue.style.marginLeft = "20%";
-    gridLocation.style.marginLeft = "10%";
-    grid.style.borderBottom = "2px solid #E1E1E1"
-    grid.style.marginBottom = "5%";
-    gridButton.style.height = '4.5vh';
-    gridButton.style.width = '13vh';
-    gridButton.style.marginTop = '1vh';
-    gridButton.style.color = '#FFFFFF';
-    gridButton.style.backgroundColor = '#323232';
-    grid.appendChild(gridDates).value;
-    grid.appendChild(gridVenue).value;
-    grid.appendChild(gridLocation).value;
-    grid.appendChild(gridButton).value;
-});
-
-window.addEventListener('DOMContentLoaded',() => {
-    const grid = document.querySelector(".shows-grid2");
-    const gridDates = document.createElement('h4');
-    const gridVenue = document.createElement("p");
-    const gridLocation = document.createElement("p");
-    const gridButton = document.createElement('button');
-    gridDates.innerText = "Tue Jul 18 2019";
-    gridVenue.innerText = "Pier 3 East";
-    gridLocation.innerText = "San Fransisco, Ca";
-    gridButton.innerText = "Buy Tickets";
-    grid.style.marginLeft = "60%";
-    grid.style.marginRight = "0";
-    grid.style.display = "flex";
-    grid.style.justifyContent = "space-between"
-    gridVenue.style.marginLeft = "22%";
-    gridLocation.style.marginLeft = "12%";
-    grid.style.borderBottom = "2px solid #E1E1E1"
-    grid.style.marginBottom = "5%";
-    gridButton.style.height = '4.5vh';
-    gridButton.style.width = '13vh';
-    gridButton.style.marginTop = '1vh';
-    gridButton.style.color = '#FFFFFF';
-    gridButton.style.backgroundColor = '#323232';
-    grid.appendChild(gridDates).value;
-    grid.appendChild(gridVenue).value;
-    grid.appendChild(gridLocation).value;
-    grid.appendChild(gridButton).value;
-});
-
-window.addEventListener('DOMContentLoaded',() => {
-    const grid = document.querySelector(".shows-grid3");
-    const gridDates = document.createElement('h4');
-    const gridVenue = document.createElement("p");
-    const gridLocation = document.createElement("p");
-    const gridButton = document.createElement('button');
-    gridDates.innerText = "Fri Jul 22 2019";
-    gridVenue.innerText = "View Loungue";
-    gridLocation.innerText = "San Fransisco, Ca";
-    gridButton.innerText = "Buy Tickets";
-    grid.style.marginLeft = "60%";
-    grid.style.marginRight = "0";
-    grid.style.display = "flex";
-    grid.style.justifyContent = "space-between"
-    gridVenue.style.marginLeft = "23.1%";
-    gridLocation.style.marginLeft = "8.5%";
-    grid.style.borderBottom = "2px solid #E1E1E1"
-    grid.style.marginBottom = "5%";
-    gridButton.style.height = '4.5vh';
-    gridButton.style.width = '13vh';
-    gridButton.style.marginTop = '1vh';
-    gridButton.style.color = '#FFFFFF';
-    gridButton.style.backgroundColor = '#323232';
-    grid.appendChild(gridDates).value;
-    grid.appendChild(gridVenue).value;
-    grid.appendChild(gridLocation).value;
-    grid.appendChild(gridButton).value;
-});
-
-window.addEventListener('DOMContentLoaded',() => {
-    const grid = document.querySelector(".shows-grid4");
-    const gridDates = document.createElement('h4');
-    const gridVenue = document.createElement("p");
-    const gridLocation = document.createElement("p");
-    const gridButton = document.createElement('button');
-    gridDates.innerText = "Sat Aug 12 2019";
-    gridVenue.innerText = "Hyatt Agency";
-    gridLocation.innerText = "San Fransisco, Ca";
-    gridButton.innerText = "Buy Tickets";
-    grid.style.marginLeft = "60%";
-    grid.style.marginRight = "0";
-    grid.style.display = "flex";
-    grid.style.justifyContent = "space-between"
-    gridVenue.style.marginLeft = "22%";
-    gridLocation.style.marginLeft = "9%";
-    grid.style.borderBottom = "2px solid #E1E1E1"
-    grid.style.marginBottom = "5%";
-    gridButton.style.height = '4.5vh';
-    gridButton.style.width = '13vh';
-    gridButton.style.marginTop = '1vh';
-    gridButton.style.color = '#FFFFFF';
-    gridButton.style.backgroundColor = '#323232';
-    grid.appendChild(gridDates).value;
-    grid.appendChild(gridVenue).value;
-    grid.appendChild(gridLocation).value;
-    grid.appendChild(gridButton).value;
-});
-
-window.addEventListener('DOMContentLoaded',() => {
-    const grid = document.querySelector(".shows-grid5");
-    const gridDates = document.createElement('h4');
-    const gridVenue = document.createElement("p");
-    const gridLocation = document.createElement("p");
-    const gridButton = document.createElement('button');
-    gridDates.innerText = "Fri Sep 05 2019";
-    gridVenue.innerText = "Moscow Center";
-    gridLocation.innerText = "San Fransisco, Ca";
-    gridButton.innerText = "Buy Tickets";
-    grid.style.marginLeft = "60%";
-    grid.style.marginRight = "0";
-    grid.style.display = "flex";
-    grid.style.justifyContent = "space-between"
-    gridVenue.style.marginLeft = "22.5%";
-    gridLocation.style.marginLeft = "6.1%";
-    grid.style.borderBottom = "2px solid #E1E1E1"
-    grid.style.marginBottom = "5%";
-    gridButton.style.height = '4.5vh';
-    gridButton.style.width = '13vh';
-    gridButton.style.marginTop = '1vh';
-    gridButton.style.color = '#FFFFFF';
-    gridButton.style.backgroundColor = '#323232';
-    grid.appendChild(gridDates).value;
-    grid.appendChild(gridVenue).value;
-    grid.appendChild(gridLocation).value;
-    grid.appendChild(gridButton).value;
-});
-
-window.addEventListener('DOMContentLoaded',() => {
-    const grid = document.querySelector(".shows-grid6");
-    const gridDates = document.createElement('h4');
-    const gridVenue = document.createElement("p");
-    const gridLocation = document.createElement("p");
-    const gridButton = document.createElement('button');
-    gridDates.innerText = "Wed Aug 11 2019";
-    gridVenue.innerText = "Pres Club";
-    gridLocation.innerText = "San Fransisco, Ca";
-    gridButton.innerText = "Buy Tickets";
-    grid.style.marginLeft = "60%";
-    grid.style.marginRight = "0";
-    grid.style.display = "flex";
-    grid.style.justifyContent = "space-between"
-    gridVenue.style.marginLeft = "19%";
-    gridLocation.style.marginLeft = "11.5%";
-    grid.style.borderBottom = "2px solid #E1E1E1"
-    gridButton.style.height = '4.5vh';
-    gridButton.style.width = '13vh';
-    gridButton.style.marginTop = '1vh';
-    gridButton.style.color = '#FFFFFF';
-    gridButton.style.backgroundColor = '#323232';
-    grid.appendChild(gridDates).value;
-    grid.appendChild(gridVenue).value;
-    grid.appendChild(gridLocation).value;
-    grid.appendChild(gridButton).value;
-});
+    grid.appendChild(gridContainer)
+    gridContainer.appendChild(gridDates).value;
+    gridContainer.appendChild(gridVenue).value;
+    gridContainer.appendChild(gridLocation).value;
+    gridContainer.appendChild(gridButton).value;
+    gridContainer.classList.add('shows-grid__container');
+    gridDates.classList.add('shows-grid__container-date');
+    gridVenue.classList.add('shows-grid__container-venue');
+    gridLocation.classList.add('shows-grid__container-location');
+    gridButton.classList.add('shows-grid__container-button');
+}
