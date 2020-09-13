91835042984a3a704211f21c4a8298c4c75a7247
Kaleb Armbrust <armbrustcopyco@gmail.com>
2020-09-12 22:43:19 -0700

    everything but the posted comments/styling

--- Sprint-two/scripts/main.js
+++ Sprint-two/scripts/main.js
@@ -1,22 +1,48 @@
-const urlComments = "https://project-1-api.herokuapp.com/comments?api_key=c96c0abc-3e8f-46ca-9145-93c0f269da4f";
-const urlDelete = "https://project-1-api.herokuapp.com/comments/:commendId?api_key=c96c0abc-3e8f-46ca-9145-93c0f269da4f"
+const urlComments = "https://project-1-api.herokuapp.com/comments?api_key=4f7a0fd5-c646-44d7-b418-094a97a9b246";
+const urlDelete = "https://project-1-api.herokuapp.com/comments/:commendId?api_key=4f7a0fd5-c646-44d7-b418-094a97a9b246"
+
 
 axios
 .get(urlComments)
 .then(res => {
-   console.log(res.data);
-
    res.data.forEach(comment => {
       createComment(comment);
    })
 })
 .catch(err => console.log(err))
 
+// axios
+// .post(urlComments, {
+   
+// })
+// .then(function (response) {
+//    console.log(response.data);
+//  })
+// .catch(function (error) {
+//    console.log(error);
+//  });
+
+const clear = () => {
+   document.querySelector('.comments-previous').innerText = '';
+ }
+
+let posts = [];
+
 const form = document.querySelector('.comments-add__form');
 form.addEventListener('submit', (e) => {
             e.preventDefault();
-            // createComment(comment);
-    })
+            newFunc();
+            // clear();
+         })
+
+
+function newFunc() {
+   const newName = form.querySelector('.comments-add__form-box1').value;
+   const newComment = form.querySelector('.comments-add__form-box2').value;
+   form.append(newName);
+   form.append(newComment);
+      }
+
 
     function createComment(comment) {
        const previous = document.querySelector('.comments-previous');
@@ -39,8 +65,4 @@ form.addEventListener('submit', (e) => {
        commentName.classList.add('comments-previous__container-name');
        commentText.classList.add('comments-previous__container-text');
     }
-// const addComment = document.querySelector(".commentsForm");
-// const list = document.querySelector('.comments-previous');
-// const name = addComment.querySelector('input[type="text"]').value;
-//     const comment = addComment.querySelector('.commentBox2').value;
 
